a43fb50c2dc745eac5ac01d09925f12cd04deb31
suraj <suraj190897@gmail.com>
2020-12-04 18:26:47 +0530

    first commit

---
+++ api/product/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+
+# Create your views here.
+
+from .serializers import ProductSerializer
+from .models import Product
+
+class ProductViewSets(viewsets.ModelViewSet):
+    queryset=Product.objects.all().order_by('id')
+    serializer_class=ProductSerializer
